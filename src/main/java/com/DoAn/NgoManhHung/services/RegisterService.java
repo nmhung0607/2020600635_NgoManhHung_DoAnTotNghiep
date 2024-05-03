5636ae499bd54aa82ae7c0c1971675a92abc24b2
nmhung0607 <ngomanhhung0607@gmail.com>
2024-05-04 00:24:05 +0700

    updated

--- src/main/java/com/DoAn/NgoManhHung/services/RegisterService.java
+++ src/main/java/com/DoAn/NgoManhHung/services/RegisterService.java
@@ -1,12 +1,12 @@
-//package com.devpro.NgoManhHungFECuoiKhoa.services;
-//import org.springframework.stereotype.Service;
-//
-//import com.devpro.NgoManhHungFECuoiKhoa.model.User;
-//
-//@Service
-//public class RegisterService extends BaseService<User>{
-//	@Override
-//	protected Class<User> clazz() {
-//		return User.class;
-//	}
-//}
+package com.DoAn.NgoManhHung.services;
+import org.springframework.stereotype.Service;
+
+import com.DoAn.NgoManhHung.model.User;
+
+@Service
+public class RegisterService extends BaseService<User>{
+	@Override
+	protected Class<User> clazz() {
+		return User.class;
+	}
+}
